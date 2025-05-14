c5b9dd8bb43c316e608cd3bf2659429bb3c21ade
mahmoud9877 <mohmoudali9877@gmail.com>
2025-05-15 01:18:14 +0300

    update

--- DataBase/connection.js
+++ DataBase/connection.js
@@ -4,16 +4,18 @@ import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.join(__dirname, "../config/.env") });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false,
+});
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 export default sequelize;
--- index.js
+++ index.js
@@ -6,22 +6,9 @@ import dotenv from "dotenv";
 //set directory dirname
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: path.join(__dirname, "./config/.env") });
-// index.js or app.js
-import sequelize from "./DataBase/connection.js";
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("MySQL connected...");
-    await sequelize.sync(); // Create tables if not exist
-    console.log("Tables synced...");
-  } catch (error) {
-    console.error("DB connection failed:", error);
-  }
-})();
-
 const app = express();
 const PORT = 5000;
+
 app.get("/", (req, res) => {
   res.send("🎉 API is working!");
 });
